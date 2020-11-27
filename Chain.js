eeb591b0fc57451ddf206f115880fb5ba5e3ae69
Spatade <72843311+Spatade@users.noreply.github.com>
2020-11-27 11:32:28 -0500

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,28 @@
+class Chain{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    fly()
+    {
+        this.chain.bodyA = null;
+    }
+
+    display(){
+        if(this.chain.bodyA)
+        {
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.bodyB.position;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
